e451d839e3fafb1e67b24169257a6d7f024456d8
DariaYashchuk <dariayashchuk2000@gmail.com>
2023-04-26 17:26:35 +0300

    v1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryWithItems = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryWithItems);
+
+galleryContainer.addEventListener("click", onItemClick);
+
+function onItemClick(evt) {
+  evt.preventDefault();
+
+  const isGalleryItem = evt.target.classList.contains("gallery__image");
+
+  if (!isGalleryItem) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryContainer.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
